dc32beb9ad37774dc512647b2552c6b0d531bc20
woojinv <oh.woojin95@gmail.com>
2022-03-17 16:34:19 -0500

    use object to declare state variables

--- js/main.js
+++ js/main.js
@@ -7,13 +7,93 @@ let currentPlayer;
 let piecesInARow;
 let gameStatusActive;
 
-let column1Height;
-let column2Height;
-let column3Height;
-let column4Height;
-let column5Height;
-let column6Height;
-let column7Height;
+const gameGrid = {
+    column1: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column2: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column3: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column4: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column5: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column6: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    column7: {
+        height: 0,
+        gameSlotStatus: {
+            1: null,
+            2: null,
+            3: null,
+            4: null,
+            5: null,
+            6: null,
+        }
+    },
+    
+}
+// let column1Height;
+// let column2Height;
+// let column3Height;
+// let column4Height;
+// let column5Height;
+// let column6Height;
+// let column7Height;
 
 let gameSlot1_1;
 let gameSlot1_2;
@@ -64,6 +144,7 @@ let gameSlot7_4;
 let gameSlot7_5;
 let gameSlot7_6;
 
+
 /*----- cached element references -----*/
 let column1El = document.querySelector('#column-1');
 let column2El = document.querySelector('#column-2');
@@ -72,6 +153,15 @@ let column4El = document.querySelector('#column-4');
 let column5El = document.querySelector('#column-5');
 let column6El = document.querySelector('#column-6');
 let column7El = document.querySelector('#column-7');
+
+let gameSlot1_1El = document.querySelector('#1-1')
+let gameSlot1_2El = document.querySelector('#1-2')
+let gameSlot1_3El = document.querySelector('#1-3')
+let gameSlot1_4El = document.querySelector('#1-4')
+let gameSlot1_5El = document.querySelector('#1-5')
+let gameSlot1_6El = document.querySelector('#1-6')
+
+
 
 /*----- event listeners -----*/
 column1El.addEventListener('click', testEventLlistener);
