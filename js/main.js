adf948d2282ea981790a2873d9b441bff9948a3b
woojinv <oh.woojin95@gmail.com>
2022-03-17 15:05:29 -0500

    declare state variables and initalize function

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
-console.log("JS linked")
+console.log("JS linked");
+
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+
+let currentPlayer;
+let piecesInARow;
+let gameStatusActive;
+
+let column1Height;
+let column2Height;
+let column3Height;
+let column4Height;
+let column5Height;
+let column6Height;
+let column7Height;
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+// INITIALIZE
+initialize();
+
+function initialize() {
+  currentPlayer = 1;
+  piecesInARow = 0;
+  gameStatusActive = true;
+
+  column1Height = 0;
+  column2Height = 0;
+  column3Height = 0;
+  column4Height = 0;
+  column5Height = 0;
+  column6Height = 0;
+  column7Height = 0;
+}
