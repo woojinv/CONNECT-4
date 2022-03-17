2383d28e3665097f32c875d6c09f7a3a515efdba
woojinv <oh.woojin95@gmail.com>
2022-03-16 21:53:06 -0500

    HTML skeleton

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log("JS linked")
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
